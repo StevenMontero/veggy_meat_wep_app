14ffe27ab7d95433e52ede5d45f039268de43e65
Michael Alpizar <michael.alpizar@ucr.ac.cr>
2021-06-17 11:07:55 -0600

    Actualizados footer y menu de vista por departamentos

--- lib/ui/pages/department_filter_page.dart
+++ lib/ui/pages/department_filter_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:veggy/ui/widgets/product_card.dart';
 
 class DepartmentFilterPage extends StatelessWidget {
@@ -10,96 +11,130 @@ class DepartmentFilterPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         height: 2000,
-        child: Expanded(
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  children: [
-                    Container(
-                        height: 180.0,
-                        child: Center(
-                          child: Text("Aquí va el banner."),
-                        )
-                    )
-                  ],
-                ),
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Container(
+                      height: 180.0,
+                      child: Center(
+                        child: Text("Aquí va el banner."),
+                      )
+                  )
+                ],
               ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Container(
-                      width: 220.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                            child: Center(
-                              child: Text('Departamentos'),
-                            ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    width: 220.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                          child: Center(
+                            child: Text('Departamentos'),
                           ),
-                          Expanded(
-                            child: ListView(
-                              children: <Widget>[
-                                SizedBox(height: 10),
-                                ListTile(
-                                  leading: Icon(Icons.whatshot),
-                                  title: Text('Carniceria'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.apps),
-                                  title: Text('Granel'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.air),
-                                  title: Text('Frutas y Verduras'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.fastfood),
-                                  title: Text('Abarrotes'),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                                  child: Center(
-                                    child: Text('Filtro'),
+                        ),
+                        Expanded(
+                          child: ListView(
+                            children: <Widget>[
+                              SizedBox(height: 10),
+                              ListTile(
+                                leading: ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                    maxWidth: 36,
+                                    maxHeight: 36,
+                                  ),
+                                  child: Image.asset(
+                                    'assets/icons/icono_carniceria.png',
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
-                                const PriceFilterWidget(),
-                              ],
-                            ),
+                                title: Text('Carniceria'),
+                              ),
+                              ListTile(
+                                leading: ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                    maxWidth: 36,
+                                    maxHeight: 36,
+                                  ),
+                                  child: Image.asset(
+                                    'assets/icons/icono_granel.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                title: Text('Granel'),
+                              ),
+                              ListTile(
+                                leading: ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                    maxWidth: 36,
+                                    maxHeight: 36,
+                                  ),
+                                  child: Image.asset(
+                                    'assets/icons/icono_frutas_y_verduras.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                title: Text('Frutas y Verduras'),
+                              ),
+                              ListTile(
+                                leading: ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                    maxWidth: 36,
+                                    maxHeight: 36,
+                                  ),
+                                  child: Image.asset(
+                                    'assets/icons/icono_licores.png',
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                title: Text('Abarrotes'),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                child: Center(
+                                  child: Text('Filtro'),
+                                ),
+                              ),
+                              const PriceFilterWidget(),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Scrollbar(
-                          child: GridView.builder(
-                              padding: EdgeInsets.symmetric(horizontal: 24),
-                              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                                  maxCrossAxisExtent: 200.0,
-                                  childAspectRatio: 0.6,
-                                  crossAxisSpacing: 1,
-                                  mainAxisSpacing: 1),
-                              itemCount: 40,
-                              itemBuilder: (BuildContext ctx, index) {
-                                return ProductCard(
-                                    title: 'Soya Chips',
-                                    subtitle: 'CRC 3000',
-                                    cornerIcon: Icons.shop,
-                                    imageUrl: '',
-                                    onPressCard: () {},
-                                    onPressButton: () {});
-                              }),
                         ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Scrollbar(
+                        child: GridView.builder(
+                            padding: EdgeInsets.symmetric(horizontal: 24),
+                            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 200.0,
+                                childAspectRatio: 0.6,
+                                crossAxisSpacing: 1,
+                                mainAxisSpacing: 1),
+                            itemCount: 40,
+                            itemBuilder: (BuildContext ctx, index) {
+                              return ProductCard(
+                                  title: 'Soya Chips',
+                                  subtitle: 'CRC 3000',
+                                  cornerIcon: Icons.shop,
+                                  imageUrl: '',
+                                  onPressCard: () {},
+                                  onPressButton: () {});
+                            }),
                       ),
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/ui/widgets/bottomBar.dart
+++ lib/ui/widgets/bottomBar.dart
@@ -1,143 +1,64 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:sign_button/sign_button.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:veggy/ui/widgets/email_text.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BottomBar extends StatelessWidget implements PreferredSizeWidget {
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 130,
-      decoration: BoxDecoration(color: Colors.grey[800]),
+      height: 100,
+      decoration: BoxDecoration(color: Colors.black),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            margin: new EdgeInsets.symmetric(horizontal: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            //alignment: Alignment.center,
+            child: Row(
               children: [
                 Container(
-                  child: Center(
-                    child: Text(
-                      '¿Quiere recibir',
-                      style: TextStyle(
-                        foreground: Paint()
-                          ..color = Colors.white,
-                      ),
+                  margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Container(
+                    child: Image.asset(
+                      'assets/images/suscribete.png',
                     ),
                   ),
                 ),
                 Container(
-                  child: Center(
-                    child: Text(
-                      'descuentos y noticias?',
-                      style: TextStyle(
-                        foreground: Paint()
-                          ..color = Colors.white,
-                      ),
+                  constraints: BoxConstraints(maxWidth: 240),
+                  child: EmailText(),
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () => _launchURL('https://www.instagram.com/veggyandmeat/'),
+                    child: Image.asset(
+                      "assets/icons/icono_instagram.png",
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () => _launchURL('https://www.facebook.com/veggy.cr'),
+                    child: Image.asset(
+                      "assets/icons/icono_facebook.png",
+                      height: 40,
+                      width: 40,
                     ),
                   ),
                 ),
               ],
-            )
+            ),
           ),
           Container(
-            height: 60.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  margin: new EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(
-                      ' Ingrese su correo electrónico ',
-                      style: TextStyle(
-                        foreground: Paint()
-                          ..color = Colors.black,
-                      ),
-                    ),
-                ),
-                Container(
-                  height: 30,
-                  margin: new EdgeInsets.symmetric(horizontal: 10.0),
-                  child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.cyan,
-                      ),
-                      onPressed: null,
-                      child: Text(
-                        ' Enviar ',
-                        style: TextStyle(
-                          foreground: Paint()
-                            ..color = Colors.white,
-                        ),
-                      ),
-                    )
-                )
-              ], //
-            ),
-          ),
-          Spacer(),
-          Align(
-            //alignment: Alignment.centerRight,
+            height: 120.0,
+            margin: new EdgeInsets.symmetric(horizontal: 20.0),
             child: Container(
-              margin: new EdgeInsets.symmetric(horizontal: 10.0),
-              //mainAxisAlignment: MainAxisAlignment.right,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: new EdgeInsets.symmetric(vertical: 10.0),
-                    height: 34.0,
-                    child: Center(
-                      child: Container(
-                        child: Image.asset('logoTemp.png'),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Center(
-                      child: Text(
-                        'Siguenos en nuestras redes sociales',
-                        style: TextStyle(
-                          foreground: Paint()
-                            ..color = Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 60,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: new EdgeInsets.symmetric(horizontal: 5.0),
-
-                          child: SignInButton(
-                            Buttons.Facebook,
-                            mini: true, onPressed: () {},
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius. circular(10.0),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: new EdgeInsets.symmetric(horizontal: 5.0),
-                          child: SignInButton(
-                            Buttons.Twitter,
-                            mini: true, onPressed: () {},
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius. circular(10.0),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+              child: Image.asset(
+                'assets/icons/icono_principal.png',
               ),
             ),
           ),
@@ -145,11 +66,14 @@ class BottomBar extends StatelessWidget implements PreferredSizeWidget {
       ),
     );
   }
-
-  BoxDecoration buildBoxDecoration() => BoxDecoration(
-      color: Colors.white,
-      boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5)]);
-
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+_launchURL(url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }
