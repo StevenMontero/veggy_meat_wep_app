26ced18377e9b6798e808f83f7aed5fc9919de9e
Marco Orozco <marco.orozco@ucrso.info>
2021-06-17 14:52:12 -0600

    shoppingCartRouter añadido

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -4,6 +4,7 @@ import 'package:veggy/ui/pages/detail/detail_page.dart';
 import 'package:veggy/ui/pages/formpage/form_preorder_page.dart';
 import 'package:veggy/ui/pages/homepage/homePage.dart';
 import 'package:veggy/ui/pages/department_filter_page.dart';
+import 'package:veggy/ui/pages/shopping%20cart/shoppingCart.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
@@ -13,6 +14,7 @@ class Flurorouter {
   static String homeRoute = '/home';
   static String formPreOrderRoute = '/preorder';
   static String departmentFilterRoute = '/departmentFilter';
+  static String shoppingCartRoute = '/shoppingCart';
 
   static Handler _detailHandler =
       Handler(handlerFunc: (context, parameters) => DetailPage());
@@ -24,17 +26,24 @@ class Flurorouter {
       Handler(handlerFunc: (context, parameters) => FormPreorderPage());
   static Handler _departmentFilterHandler =
       Handler(handlerFunc: (context, parameters) => DepartmentFilterPage());
+  static Handler _shoppingCartHandler =
+      Handler(handlerFunc: (context, parameters) => ShoppingCart());
+
   static void setupRouter() {
     router.define(rootRoute,
-        handler: _homeHandler , transitionType: TransitionType.none);
+        handler: _homeHandler, transitionType: TransitionType.none);
     router.define(detailRoute,
         handler: _detailHandler, transitionType: TransitionType.none);
     router.define(formPreOrderRoute,
         handler: _formPreOrderHandler, transitionType: TransitionType.fadeIn);
-	router.define(formPreOrderRoute,
+    router.define(formPreOrderRoute,
         handler: _formPreOrderHandler, transitionType: TransitionType.fadeIn);
     router.define(formPreOrderRoute,
         handler: _formPreOrderHandler, transitionType: TransitionType.none);
+    router.define(shoppingCartRoute,
+        handler: _departmentFilterHandler, transitionType: TransitionType.none);
+    router.define(shoppingCartRoute,
+        handler: _shoppingCartHandler, transitionType: TransitionType.none);
 
     router.notFoundHandler = _notFoundHandler;
   }
