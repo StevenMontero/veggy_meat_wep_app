f49f548f1d0f8f20c2c7c2fc76e326093005dd5d
Michael Alpizar <michael.alpizar@ucr.ac.cr>
2021-07-07 11:51:41 -0600

    Agregada imagen de producto no disponible.

--- lib/data/production/repositories/product_repository.dart
+++ lib/data/production/repositories/product_repository.dart
@@ -21,9 +21,9 @@ class ProductRepository {
           await _productsCategoryRef.doc(startDocumentId).get();
       _resul = await _productsCategoryRef
           .startAfterDocument(_startDocument)
-          .where('active', isEqualTo: '1')
           .limit(10)
           .get();
+      //.where('active', isEqualTo: '1')
     } else {
       _resul = await _productsCategoryRef.limit(10).get();
     }
