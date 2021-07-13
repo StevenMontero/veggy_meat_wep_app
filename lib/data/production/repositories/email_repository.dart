8e06bde741b4fe65b16d5146086ebe23d537f372
Michael Alpizar <michael.alpizar@ucr.ac.cr>
2021-07-12 23:23:12 -0600

    Agregado almacenado de correos via footer.

---
+++ lib/data/production/repositories/email_repository.dart
@@ -0,0 +1,34 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:veggy/domain/models/email.dart';
+
+class EmailRepository {
+  late CollectionReference<Map<String, dynamic>> _emailListReference;
+  CollectionReference correosElectronicos = FirebaseFirestore.instance.collection('CORREOS ELECTRONICOS');
+
+
+  Future<List<Email>> getEmails() async{
+
+    _emailListReference = FirebaseFirestore.instance.collection('CORREOS ELECTRONICOS');
+    final QuerySnapshot<Map<String, dynamic>> _result;
+
+    _result = await _emailListReference.get();
+
+    return _result.docs.map((e) => Email.fromJson(e.data())).toList();
+
+  }
+
+  Future<bool> saveEmail(String receivedEmail) async{
+
+    bool returnValue = false;
+
+    correosElectronicos.add({
+      'correo': receivedEmail,
+    })
+        .then((value) => print("Email added"))
+        .catchError((error) => print("Failed to add email: $error"));
+
+    return returnValue;
+  }
+}
