5cee86607eb1dae71d7b92d808b1ae0f2e23aa2a
STEVEN GERARDO MONTERO MUNOZ <steven.monteromunoz@ucr.ac.cr>
2021-07-15 00:27:42 -0600

    error post arreglado

--- lib/data/production/repositories/qupos_server_repository.dart
+++ lib/data/production/repositories/qupos_server_repository.dart
@@ -8,20 +8,19 @@ class QuposRepository {
   void postPreventa(PreOrder preOrder) async {
     print(jsonEncode(preOrder.toJson()));
     String basicAuth = 'Basic ' + base64Encode(utf8.encode('pruebas:123456'));
-    final response = await dioClient.post(
-        'http://186.177.135.3:45570/api/Preventas',
-        data: jsonEncode(preOrder.toJson()),
-        options: Options(headers: {
-          'authorization': basicAuth,
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/x-www-form-urlencoded"
-        }));
+    final response =
+        await dioClient.post('http://186.177.135.3:45570/api/Preventas',
+            data: jsonEncode(preOrder.toJson()),
+            options: Options(headers: {
+              'authorization': basicAuth,
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json"
+            }));
 
     if (response.statusCode == 200) {
       print(response.data);
-      print('ok se envio bien');
+    } else {
+      print('ERROR : response.statusCode \n${response.data}');
     }
-    print('///////////Final responce////////////////');
-    print(response.data);
   }
 }
